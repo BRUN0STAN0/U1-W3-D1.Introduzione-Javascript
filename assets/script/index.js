18eda3be8a9acdc25e0a0ef9f147cfb18e20e369
Bruno <brunostano00@protonmail.com>
2022-09-12 17:06:10 +0200

    exercise complete

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,2 +1,17 @@
-alert('Usa il pop up per dare dei messaggi');
-window.alert('Sto studiando JS');
+let found = false;
+let saveOne = document.getElementById("text-one").innerHTML;
+
+function crossOutPhrase() {
+    if (found == false) {
+        document.getElementById("text-one").innerHTML = "<del>" + document.getElementById("text-one").innerHTML + "</del>";
+        document.getElementById("text-one").style.textShadow = "1px 1px yellow";
+        document.getElementById("button-one").innerHTML = "X";
+        found = true;
+    } else {
+        document.getElementById("text-one").innerHTML = saveOne;
+        document.getElementById("text-one").style.textShadow = "none";
+        document.getElementById("button-one").innerHTML = "";
+        found = false;
+    }
+
+}
